5e8dd4f0dd72204261be537c6d35ee5a2fe916bc
zhenish200 <chempjeka@gmail.com>
2024-08-21 15:49:23 +0600

    fghjkl;

---
+++ str.py
@@ -0,0 +1,30 @@
+#lower
+#тамгалардын баарын кичирейтет
+print('KYrgYzStaN'.lower())
+
+#upper
+#тамгалардын баарын чонойтот
+print('KYRGYZSTAN'.upper())
+
+#title, capitalize
+#баш тамганы чон кылып калганын кичирейтет
+print('Kyrgyzstan'.title())
+
+#len
+#создо же тырмакчадан тырмакчага чейин канча тамга же места бар экенин эсептейт
+print(len('kyrgyzstan'))
+
+#count
+#создон бир тамганы алып канча окшош тамга бар экенин санайт
+print('kyrgyzstan'.count('y'))
+
+#replace
+#орун алмаштыруу
+print('kyrgyzstan'.replace('y', 'o'))
+
+#split
+#оз озунчо тырмакчага алып коет
+print('Asan Aidana Anara'.split())
+#join
+#тескерисинче болунгонду бир кыдып жазат
+print('_'.join(['Asan', 'Aidana', 'Anara']))
